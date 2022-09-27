985e28fd4167e74cc4a5c89444b5135febe9fdbe
OwenWhalley <113034255+OwenWhalley@users.noreply.github.com>
2022-09-27 09:14:52 -0400

    Added comment

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 let Current_Temperature = 0
+// Created by: Owen Whalley
+// Created on date: 2022/09/27
+// 
+// Finding current temperature and refreshes every second.
 basic.forever(function () {
     Current_Temperature = input.temperature()
     basic.showNumber(Current_Temperature)
